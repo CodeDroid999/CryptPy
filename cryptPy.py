956cac922a934cb1b05c66532ffd5ec19621fbde
CodeDroid999 <johnmwega2002@gmail.com>
2022-11-02 22:33:20 +0300

    keys.png

--- cryptPy.py
+++ cryptPy.py
@@ -6,7 +6,14 @@ import os
 def main_screen():
         screen=Tk()
         screen.geometry("375x398")
+
+        #icon
+        image_icon=PhotoImage(file="keys.png")
+        screen.iconphoto(false,image_icon)
         screen.title("PctApp")
+        
+        Label(text="Enter text for encryption", fg="black",font=("calbri,13")).place(x=10,y=10)
+        
         screen.mainloop()
 
 main_screen()      
