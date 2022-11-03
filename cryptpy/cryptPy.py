093802e0e150acbd8c8d178d7cd5ee2c464be538
CodeDroid999 <johnmwega2002@gmail.com>
2022-11-03 11:36:13 +0300

    quick fix

--- cryptpy/cryptPy.py
+++ cryptpy/cryptPy.py
@@ -1,3 +1,4 @@
+#import libraries
 from tkinter import *
 from tkinter import messagebox
 import base64
@@ -5,31 +6,7 @@ import os
 
 
 
-def decrypt():
-    password=code.get()
-    
-    if password=="1234":
-        screen2=Toplevel(screen)
-        screen2.title("decryption")
-        screen2.geometry("400x200")
-        screen2.configure(bg="#00bd56")
 
-        message=text1.get(1.0,END)
-        decode_message=message.encode("ascii")
-        base64_bytes=base64.b64decode(decode_message)
-        decrypt=base64_bytes.decode("ascii")
-
-        Label(screen2,text="ENCRYPT",font="arial",fg="white",bg="#00bd56").place(x=10,y=0)
-        text2=Text(screen2,font="roboto 10",bg="white",relief=GROOVE,wrap=WORD,bd=0)
-        text2.place(x=10,y=40,width=380,height=150)
-
-        text2.insert(END,decrypt)
-
-    elif password =="":
-        messagebox.showerror("encryption","Input password")
-        
-    else:
-        messagebox.showerror("encryption","Inavlid password")
 
 
 def encrypt():
@@ -58,12 +35,38 @@ def encrypt():
     else:
         messagebox.showerror("encryption","Inavlid password")
 
+def decrypt():
+    password=code.get()
+    
+    if password=="1234":
+        screen2=Toplevel(screen)
+        screen2.title("decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="#00bd56")
+
+        message=text1.get(1.0,END)
+        decode_message=message.encode("ascii")
+        base64_bytes=base64.b64decode(decode_message)
+        decrypt=base64_bytes.decode("ascii")
+
+        Label(screen2,text="ENCRYPT",font="arial",fg="white",bg="#00bd56").place(x=10,y=0)
+        text2=Text(screen2,font="roboto 10",bg="white",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+
+        text2.insert(END,decrypt)
+
+    elif password =="":
+        messagebox.showerror("encryption","Input password")
+        
+    else:
+        messagebox.showerror("encryption","Inavlid password")        
+
 
 def main_screen():
         global screen
         global code 
         global text1
-
+        #screen
         screen=Tk()
         screen.geometry("375x398")
 
@@ -71,30 +74,27 @@ def main_screen():
         image_icon=PhotoImage(file="keys.png")
         screen.iconphoto(False,image_icon)
         screen.title("CryptPy App")
-
+        #reset function
         def reset():
             code.set("")
             text1.delete(1.0,END)
 
 
-        
+        #text area for text to be encrypted
         Label(text="Enter text for encryption", fg="black",font=("calbri,13")).place(x=10,y=10)
         text1=Text(font="Roboto 20", bg="white", relief=GROOVE,wrap=WORD,bd=0)
         text1.place(x=10,y=50,width=355,height=100)
-
+        #text area for the secret key
         Label(text="Enter secret key for encryption and decryption", fg="black",font=("calbri,13")).place(x=10,y=170)
-        #text1=Text(font="Roboto 20", bg="white", relief=GROOVE,wrap=WORD,bd=0)
-        #text1.place(x=10,y=50,width=355,height=100)
         
         code=StringVar()
         Entry(textvariable=code,width=19,bd=0,font=("aril",25),show="*").place(x=10,y=200)
-
+        #encrypt button
         Button(text="ENCRYPT",height="2",width=23,bg="#ed3833",fg="white",bd=0,command=encrypt).place(x=10,y=250)
+        #decrypt button
         Button(text="DECRYPT",height="2",width=23,bg="#00bd56",fg="white",bd=0,command=decrypt).place(x=200,y=250)
+        #rest button
         Button(text="RESET",height="2",width=50,bg="#1089ff",fg="white",bd=0, command=reset).place(x=10,y=300)
-
-
-
         screen.mainloop()
 
 main_screen()      
